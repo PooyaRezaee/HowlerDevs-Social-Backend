509229d17dbf5cb322546a60564833c57fa39dfc
PooyaRezaei <pooya.rezaee.official@gmail.com>
2025-07-27 00:48:29 +0330

    finalize admin panels for Connection and Content models

--- apps/connect/admin.py
+++ apps/connect/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from .models import Connection
 
-# Register your models here.
+
+@admin.register(Connection)
+class ConnectionAdmin(admin.ModelAdmin):
+    list_display = ("id", "requester_link", "receiver_link", "is_accept", "created_at")
+    list_filter = ("is_accept", "created_at")
+    search_fields = (
+        "requester__username",
+        "receiver__username",
+    )
+    list_editable = ("is_accept",)
+    ordering = ("-created_at",)
+
+    def requester_link(self, obj):
+        url = reverse("admin:account_user_change", args=[obj.requester.pk])
+        return format_html('<a href="{}">{}</a>', url, obj.requester.username)
+
+    requester_link.short_description = "Requester"
+    requester_link.admin_order_field = "requester__username"
+
+    def receiver_link(self, obj):
+        url = reverse("admin:account_user_change", args=[obj.receiver.pk])
+        return format_html('<a href="{}">{}</a>', url, obj.receiver.username)
+
+    receiver_link.short_description = "Receiver"
+    receiver_link.admin_order_field = "receiver__username"
--- apps/content/admin.py
+++ apps/content/admin.py
@@ -1,12 +1,54 @@
 from django.contrib import admin
-from .models import MediaContent, Post
+from django.utils.html import format_html
+from .models import MediaContent, Post, Hashtag, Content
 
 
-@admin.register(MediaContent)
-class MediaAdmin(admin.ModelAdmin):
+class BaseContentAdmin(admin.ModelAdmin):
     exclude = ["likes"]
+    readonly_fields = ["likes_count", "created_at", "updated_at"]
+    list_display = ("id", "owner", "likes_count", "created_at", "thumbnail_preview")
+    list_filter = ("created_at", "owner")
+    search_fields = ("description", "owner__username", "owner__email")
+    ordering = ("-created_at",)
+
+    def likes_count(self, obj):
+        return obj.likes.count()
+
+    likes_count.short_description = "Likes"
+
+    def thumbnail_preview(self, obj):
+        if obj.thumbnail:
+            return format_html(
+                '<img src="{}" width="60" height="60" style="object-fit: cover;" />',
+                obj.thumbnail.url,
+            )
+        return "-"
+
+    thumbnail_preview.short_description = "Thumbnail"
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    exclude = ["likes"]
+class PostAdmin(BaseContentAdmin):
+    pass
+
+
+@admin.register(MediaContent)
+class MediaAdmin(BaseContentAdmin):
+    list_display = BaseContentAdmin.list_display + ("media_type", "file_name")
+
+    def file_name(self, obj):
+        return obj.file.name.split("/")[-1] if obj.file else "-"
+
+    file_name.short_description = "File"
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "content_count")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+    def content_count(self, obj):
+        return obj.contents.count()
+
+    content_count.short_description = "Used in"
